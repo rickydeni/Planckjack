c77e886d718ee90aaeee653e2fc21bb39316346b
De Ninno Riccardo <74534634+rickydeni@users.noreply.github.com>
2022-04-16 21:57:58 +0200

    Add files via upload

---
+++ Planckjack/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import view.Finestra;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Finestra f = new Finestra();
+		
+	}
+
+}
